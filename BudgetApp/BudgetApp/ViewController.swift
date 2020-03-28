14c90968bbc2f26976e1dd9797e6a701b9fbfc4b
ChrisKozich <christopherkozich@gmail.com>
2020-03-28 14:02:24 -0400

    Initial Commit

---
+++ BudgetApp/BudgetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by Chris Kozich on 3/28/20.
+//  Copyright © 2020 Chris Kozich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
