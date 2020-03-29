c9f1d1035a3f69b31de60174102fbf738f9ec44e
Evan Juncal <35431775+ecjuncal@users.noreply.github.com>
2020-03-29 12:31:43 -0400

    Moved backend to own repo to get cloud build working properly

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    # General Config
-    TESTING = True
-    DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///localdb.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
